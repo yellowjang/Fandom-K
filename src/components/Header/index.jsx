12fc39958e4c7828598849296cc10f2601bfcdfb
binyongbin <kyb2475@gmail.com>
2024-06-11 21:24:22 +0900

    Feat: Header 컴포넌트 생성

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+import styles from './styles.module.scss';
+import logo from '../../assets/images/logo/logo.png';
+import profileImage from '../../assets/images/header/profile_img.svg';
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div></div>
+        <Link to='/list'>
+          <img
+            className={styles.logo}
+            src={logo}
+            alt='팬덤 케이'
+            width={168}
+            height={32}
+          />
+        </Link>
+        <Link>
+          <img
+            className={styles.profile}
+            src={profileImage}
+            alt='프로필 이미지'
+            width={32}
+            height={32}
+          />
+        </Link>
+      </header>
+    </div>
+  );
+}
+
+export default Header;

841d5a2abb6a7328639cbb60afdbbf2598d3a535
binyongbin <kyb2475@gmail.com>
2024-06-12 21:23:00 +0900

    Refactor: Landing 페이지 리팩토링

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -3,27 +3,31 @@ import logoImage from '../../assets/images/logo/logo.png';
 import Description from './components/Description';
 import { Link } from 'react-router-dom';
 
+const StartButton = () => (
+  <div className={styles['start-button']}>
+    <Link to='/list'>
+      <button>지금 시작하기</button>
+    </Link>
+  </div>
+);
+
 function Landing() {
   return (
     <div className={styles['container']}>
-        <div className={styles['main']}>
-          <div className={styles['description']}>
-            <p>
-              내가 좋아하는 아이돌을 <br />
-              가장 <span>쉽게 덕질</span> 하는 방법
-            </p>
-            <Link to='/list'>
-              <img src={logoImage} alt='팬덤 케이' width={509} height={97} />
-            </Link>
-            <div className={styles['start-button']}>
-              <Link to='/list'>
-                <button>지금 시작하기</button>
-              </Link>
-            </div>
-          </div>
+      <div className={styles['main']}>
+        <div className={styles['description']}>
+          <p>
+            내가 좋아하는 아이돌을 <br />
+            가장 <span>쉽게 덕질</span> 하는 방법
+          </p>
+          <Link to='/list'>
+            <img src={logoImage} alt='팬덤 케이' width={509} height={97} />
+          </Link>
+          <StartButton />
         </div>
-        <Description />
       </div>
+      <Description />
+    </div>
   );
 }
 

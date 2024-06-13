68b1a861709cdd96bd5c2c88ec584de76a2b8faf
KangHyoSung <godkingdawn@naver.com>
2024-06-13 17:29:45 +0900

    Feat : 아이돌 선택 리스트 map 추가

--- src/pages/Mypage/component/IdolCard/index.jsx
+++ src/pages/Mypage/component/IdolCard/index.jsx
@@ -1,13 +1,16 @@
 import styles from './styles.module.scss';
 import Picture from '../../../../assets/images/asmrz_danaka.png'
 
-function IdolCard() {
+
+function IdolCard({item}) {
     return(
         <div className={styles['idol-card']}>
-            <img src={Picture} className={styles['idol-card-img']} alt="아이돌사진" />
-            <div className={styles['idol-info']}>
-                <h3>이름</h3>
-                <h3>그룹명</h3>
+            <div className={styles['idol-image-container']}>
+            <img src={item.profilePicture} className={styles['idol-image']} alt="아이돌사진" />
+            </div>
+            <div className={styles['idol-info-container']}>
+                <h3 className={styles['group']}>{item.group}</h3>
+                <h3 className={styles['name']}>{item.name}</h3>
             </div>
         </div>
         
--- src/pages/Mypage/component/SelectIdolList/index.jsx
+++ src/pages/Mypage/component/SelectIdolList/index.jsx
@@ -1,17 +1,26 @@
 import IdolCard from '../IdolCard';
+import idolItem from './mock.json';
 import styles from './styles.module.scss';
 
+function SelectIdolList({ items }) {
+  items = idolItem;
+  return (
+    <div className={styles['selcet-idol']}>
+      <div className={styles['seltct-idol-title']}>
+        <h3>관심있는 아이돌을 추가해보세요.</h3>
+      </div>
 
-function SelectIdolList() {
-    return (
-        <>
-        <div>
-            <h3>내가관심있는 아이돌을 추가해보세요.</h3>
-        </div>
-            <IdolCard />
-        </> 
-    )
+      <div className={styles['list-container']}>
+        {items?.map((item) => {
+          return <IdolCard key={item.id} item={item} />;
+        })}
+      </div>
+
+      <div>
+        <button></button>
+      </div>
+    </div>
+  );
 }
-
 
 export default SelectIdolList;

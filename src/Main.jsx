c49194e3d3ff9097f4e5f8d1d99d070fcece4b3a
LeeTJ <lees@lees.com>
2024-06-13 12:17:19 +0900

    Feat: 테스트 페이지 추가

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from '@/App';
 import NotFound from '@/pages/NotFound';
+import TestPage from '@/pages/TestPage';
 
 function Main() {
   return (
@@ -9,6 +10,7 @@ function Main() {
         <Route path='/' element={<App />}>
           {/* <Route index element={<List />} /> */}
           {/* <Route path='mypage' element={<MyPage />} /> */}
+          <Route path='test' element={<TestPage />} />
           <Route path='*' element={<NotFound />} />
         </Route>
       </Routes>

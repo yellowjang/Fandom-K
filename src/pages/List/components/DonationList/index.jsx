0d3f07a85c1296b8aacc57b07128506e4c308925
binyongbin <kyb2475@gmail.com>
2024-06-14 14:47:44 +0900

    Feat: 후원하기 API 연동

--- src/pages/List/components/DonationList/index.jsx
+++ src/pages/List/components/DonationList/index.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react';
 import DonationElement from './DonationElement';
 import styles from './styles.module.scss';
 import arrowLeft from '@/assets/buttons/pagination_arrow_left.png';
 import arrowRight from '@/assets/buttons/pagination_arrow_right.png';
+import { getDonations } from '@/services/api/donations';
 
 function DonationList() {
+  const [donations, setDonations] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { list } = await getDonations();
+      setDonations(list);
+    };
+
+    
+    fetchData();
+    }, []);
+
   return (
     <>
       <div className={styles['donation-list']}>
@@ -19,10 +33,9 @@ function DonationList() {
             <p className={styles['list-title']}>후원을 기다리는 조공</p>
 
             <div className={styles['components-wrapper']}>
-              <DonationElement />
-              <DonationElement />
-              <DonationElement />
-              <DonationElement />
+              {donations.map((donation) => (
+                <DonationElement key={donation.id} donation={donation} />
+              ))}
             </div>
           </div>
           <button className={styles['arrow-button']}>

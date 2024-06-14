2f8f75fc943bef8145854b378738cdca12efd10c
KangHyoSung <godkingdawn@naver.com>
2024-06-14 13:21:27 +0900

    Chore: favoriteIdolPage 진행 중 pull 받으려 커밋

--- src/pages/Mypage/component/FavoriteIdol/index.jsx
+++ src/pages/Mypage/component/FavoriteIdol/index.jsx
@@ -1,13 +1,26 @@
 import styles from './styles.module.scss';
+import logoImage from '@/assets/images/logo/logo.png';
 
+function FavoriteIdol() {
+  return (
+    <div className={styles['favorite-idol']}>
+      <div className={styles['favorite-idol-title']}>
+        <h3>내가관심있는 아이돌</h3>
+      </div>
+      <div className={styles['favorite-add-info']}>
+      <img
+          src={logoImage}
+          className={styles['logo-image']}
+          alt='로고이미지'
+          width='120px'
+          height='23'
+        />
+        <h3>좋아하는 아이돌을 추가해주세요</h3>
+      </div>
 
-function FavoriteIdol () {
-    return (
-        <div>
-            <h3>내가관심있는 아이돌</h3>
-        </div>
-    )
+      <div className={styles['bottom-line']}></div>
+    </div>
+  );
 }
-
 
 export default FavoriteIdol;
--- src/pages/Mypage/component/IdolCard/index.jsx
+++ src/pages/Mypage/component/IdolCard/index.jsx
@@ -1,21 +1,22 @@
 import styles from './styles.module.scss';
-import Picture from '../../../../assets/images/asmrz_danaka.png'
+import Picture from '../../../../assets/images/asmrz_danaka.png';
 
-
-function IdolCard({item}) {
-    return(
-        <div className={styles['idol-card']}>
-            <div className={styles['idol-image-container']}>
-            <img src={item.profilePicture} className={styles['idol-image']} alt="아이돌사진" />
-            </div>
-            <div className={styles['idol-info-container']}>
-                <h3 className={styles['group']}>{item.group}</h3>
-                <h3 className={styles['name']}>{item.name}</h3>
-            </div>
-        </div>
-        
-    
-    )
+function IdolCard({ item }) {
+  return (
+    <div className={styles['idol-card']}>
+      <div className={styles['idol-image-container']}>
+        <img
+          src={item.profilePicture}
+          className={styles['idol-image']}
+          alt='아이돌사진'
+        />
+      </div>
+      <div className={styles['idol-info-container']}>
+        <h3 className={styles['group']}>{item.group}</h3>
+        <h3 className={styles['name']}>{item.name}</h3>
+      </div>
+    </div>
+  );
 }
 
 export default IdolCard;

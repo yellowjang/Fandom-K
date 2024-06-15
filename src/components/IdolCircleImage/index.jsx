9e2853629b20049d5537cedd9abb3646fe532a62
binyongbin <kyb2475@gmail.com>
2024-06-15 11:14:18 +0900

    Refactor: IdolCircleImage prop 받을 위치 수정

--- src/components/IdolCircleImage/index.jsx
+++ src/components/IdolCircleImage/index.jsx
@@ -5,12 +5,12 @@ function IdolCircleImage({ imgUrl, idolName, size = 70 }) {
     <div className={styles['idol-circle-image']}>
       <div
         className={styles['image-container']}
+        style={{ width: size, height: size }}
       >
         <img
           className={styles['image']}
           src={imgUrl}
           alt={`${idolName} 사진`}
-          style={{ width: size, height: size }}
         />
       </div>
     </div>

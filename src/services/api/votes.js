2603e10f5fca0c1036f2dbe333d42cfb9766ab9b
binyongbin <kyb2475@gmail.com>
2024-06-15 17:25:42 +0900

    Feat: 투표하기, 크레딧 후원하기 API 연동 및 API 예외처리 완료

---
+++ src/services/api/votes.js
@@ -0,0 +1,24 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export async function postVotes(data) {
+  try {
+    const response = await fetch(`${BASE_URL}/votes`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error('투표에 실패했습니다.');
+    }
+
+    const body = await response.json();
+
+    return body;
+  } catch (error) {
+    return { error: error.message };
+  }
+}

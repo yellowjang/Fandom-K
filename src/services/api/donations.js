4f0ebd932b9825c27ffd8cb660e9984fa29a4642
yongb2n <144973656+yongb2n@users.noreply.github.com>
2024-06-15 17:27:25 +0900

    Feature/2-투표하기, 크레딧 후원하기 API 연동 및 API 예외 처리 (#21)
    * Fix: 변수 에러 해결
    
    * Refactor: Header link path 변경
    
    * Chore: 코드 포맷팅
    
    * Feat: 투표하기, 크레딧 후원하기 API 연동 및 API 예외처리 완료

--- src/services/api/donations.js
+++ src/services/api/donations.js
@@ -1,13 +1,45 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function getDonations() {
-  const response = await fetch(`${BASE_URL}/donations`, {
-    headers: {
-      Accept: 'application/json',
-    },
-    method: 'GET',
-  });
-  const body = await response.json();
+  try {
+    const response = await fetch(`${BASE_URL}/donations`, {
+      headers: {
+        Accept: 'application/json',
+      },
+      method: 'GET',
+    });
 
-  return body;
+    if (!response.ok) {
+      throw new Error('데이터를 불러오지 못했습니다.');
+    }
+
+    const body = await response.json();
+
+    return body;
+  } catch (error) {
+    return { error: error.message };
+  }
+}
+
+export async function creditDonation({ id, data }) {
+  try {
+    const response = await fetch(`${BASE_URL}/donations/${id}/contribute`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'PUT',
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error('후원에 실패했습니다.');
+    }
+
+    const responseBody = await response.json();
+
+    return responseBody;
+  } catch (error) {
+    return { error: error.message };
+  }
 }

1903b00079ad049f67a3398aab95a40606d3180d
binyongbin <kyb2475@gmail.com>
2024-06-17 13:57:26 +0900

    Feat: 랜딩 페이지 Framer motion 애니메이션 적용

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import styles from './styles.module.scss';
 import logoImage from '../../assets/images/logo/logo.png';
 import Description from './components/Description';
@@ -6,22 +7,44 @@ import { Link } from 'react-router-dom';
 function Landing() {
   return (
     <div className={styles['container']}>
-      <div className={styles['main']}>
+      <motion.div
+        className={styles['main']}
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+      >
         <div className={styles['description']}>
-          <p>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 0.5 }}
+          >
             내가 좋아하는 아이돌을 <br />
             가장 <span>쉽게 덕질</span> 하는 방법
-          </p>
+          </motion.p>
           <Link to='/list'>
-            <img src={logoImage} alt='팬덤 케이' width={509} height={97} />
+            <motion.img
+              src={logoImage}
+              alt='팬덤 케이'
+              width={509}
+              height={97}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 1.5 }}
+            />
           </Link>
-          <div className={styles['start-button']}>
+          <motion.div
+            className={styles['start-button']}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 1.5 }}
+          >
             <Link to='/list'>
               <button>지금 시작하기</button>
             </Link>
-          </div>
+          </motion.div>
         </div>
-      </div>
+      </motion.div>
       <Description />
     </div>
   );

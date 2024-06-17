db2423c7a4b03ec141598bd285a648df05b2f1e6
yeram7591 <166521974+yeram7591@users.noreply.github.com>
2024-06-17 21:53:33 +0900

    Feature/5 modal (#24)
    * Refactor: 모달 컴포넌트 body 태그 내 최하단 배치
    
    * feat: 모달 창 띄울 시 스크롤 방지 기능 추가

--- src/pages/List/components/MyCredit/index.jsx
+++ src/pages/List/components/MyCredit/index.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import style from './styles.module.scss';
 import creditImg from '@/assets/images/img_diamond.png';
 import CreditChargeModal from '../Modal/CreditChargeModal';
+import ModalPortal from '../Modal/components/ModalPortal';
+import { disableScroll, activateScroll } from '../Modal/components/ModalScroll';
 
 const MyCredit = () => {
   const [credit, setCredit] = useState('0');
@@ -11,6 +13,16 @@ const MyCredit = () => {
     const currentCredits = localStorage.getItem('credits') || '0';
     setCredit(currentCredits);
   }, []);
+
+  useEffect(() => {
+    if (isModalOpen) {
+      const currentScrollY = disableScroll();
+
+      return () => {
+        activateScroll(currentScrollY);
+      };
+    }
+  }, [isModalOpen]);
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -26,7 +38,7 @@ const MyCredit = () => {
 
   return (
     <>
-      <section className={style['container']}>
+      <div className={style['container']}>
         <div className={style['credit-state']}>
           <p>내 크레딧</p>
           <div className={style['credit-money']}>
@@ -35,12 +47,14 @@ const MyCredit = () => {
           </div>
         </div>
         <button onClick={openModal}>충전하기</button>
-      </section>
-      <CreditChargeModal
-        isModalOpen={isModalOpen}
-        closeModal={closeModal}
-        updateCredit={updateCredit}
-      />
+      </div>
+      <ModalPortal>
+        <CreditChargeModal
+          isModalOpen={isModalOpen}
+          closeModal={closeModal}
+          updateCredit={updateCredit}
+        />
+      </ModalPortal>
     </>
   );
 };

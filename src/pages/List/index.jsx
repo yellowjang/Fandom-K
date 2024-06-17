3290aa6abd4ce0522177c9ae6a2b74fda9ab9241
장아영 <jang-ayoung@jang-ayeong-ui-MacBookPro.local>
2024-06-17 21:42:15 +0900

    Feat: 적응형/반응형 화면 구현

--- src/pages/List/index.jsx
+++ src/pages/List/index.jsx
@@ -1,15 +1,36 @@
+import { useState, useEffect } from 'react';
 import styles from './styles.module.scss';
 import MyCredit from './components/MyCredit';
 import DonationList from './components/DonationList';
+import DonationListAdaptive from './components/DonationListAdaptive';
 import MonthlyChart from './components/MonthlyChart';
 
 function List() {
+  const [isWideScreen, setIsWideScreen] = useState(window.innerWidth >= 1200);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(window.innerWidth >= 1200);
+    };
+
+    // Initialize the state based on the current window width
+    handleResize();
+
+    // Add the event listener
+    window.addEventListener('resize', handleResize);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className={styles['list-page']}>
       <header />
       <main>
         <MyCredit />
-        <DonationList />
+        {isWideScreen ? <DonationList /> : <DonationListAdaptive />}
         <MonthlyChart />
       </main>
       <footer />

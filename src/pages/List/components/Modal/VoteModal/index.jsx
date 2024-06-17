fcb38a1084dfeb0cbd84660d5e55680dd47fa99e
engdawn <kanghansol97@gmail.com>
2024-06-17 17:05:47 +0900

    feat: 투표 모달창 내 아이돌 리스트 구현

--- src/pages/List/components/Modal/VoteModal/index.jsx
+++ src/pages/List/components/Modal/VoteModal/index.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './styles.module.scss';
 import closeIcon from '@/assets/icons/ic_close.svg';
-import idolImg from '@/assets/images/asmrz_danaka.png';
 import chartIcon from '@/assets/icons/ic_chart.svg';
 import checkIcon from '@/assets/icons/ic_check.svg';
+import ChartList from './ChartList';
 
-function VoteModal() {
+function VoteModal({ items }) {
   const [modal, setModal] = useState(false);
 
   const toggleModal = () => {
@@ -13,9 +13,9 @@ function VoteModal() {
   };
 
   if (modal) {
-    document.body.classList.add('active-modal');
+    document.documentElement.style.overflow = 'hidden';
   } else {
-    document.body.classList.remove('active-modal');
+    document.documentElement.style.overflow = '';
   }
 
   return (
@@ -40,21 +40,9 @@ function VoteModal() {
               </button>
             </header>
             <main>
-              <label className={styles['list']}>
-                <div className={styles['list-content']}>
-                  <img
-                    className={styles['idol-img']}
-                    src={idolImg}
-                    alt='아이돌 이미지'
-                  />
-                  <h1>1</h1>
-                  <div className={styles['text-content']}>
-                    <h1>뉴진스 민지</h1>
-                    <h2>204,000표</h2>
-                  </div>
-                </div>
-                <input type='radio' />
-              </label>
+              <div className={styles['list-container']}>
+                <ChartList items={items} />
+              </div>
             </main>
             <footer>
               <button>투표하기</button>

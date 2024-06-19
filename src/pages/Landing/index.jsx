f94a30378908283c0dd35e08dee695f117a69877
yongb2n <144973656+yongb2n@users.noreply.github.com>
2024-06-19 15:25:44 +0900

    [고용빈]Feature/2-투표하기 기능 추가 (#31)
    * Fix: 변수 에러 해결
    
    * Refactor: Header link path 변경
    
    * Chore: 코드 포맷팅
    
    * Feat: 투표하기, 크레딧 후원하기 API 연동 및 API 예외처리 완료
    
    * Refactor: Header 컴포넌트 OCP 원칙 적용한 코드로 리팩토링
    
    * Feat: 슬라이드 개수 상수로 선언
    
    * Feat: 인피니티 슬라이드 기능 적용
    
    * Refactor: 랜딩 페이지 StartButton 컴포넌트 제거
    
    * Refactor: 다음 슬라이드로 넘어가는 시간 변경 3초 -> 2초
    
    * Feat: Header 파일 구조 변경 및 경로 인덱스화
    
    * Feat: 랜딩 페이지 Framer motion 애니메이션 적용
    
    * Refactor: 애니메이션 설정 분리 및 반복되는 코드 분리
    
    * Feat: 투표하기 기능 추가

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -1,50 +1,42 @@
-import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
 import logoImage from '../../assets/images/logo/logo.png';
 import Description from './components/Description';
-import { Link } from 'react-router-dom';
+import AnimatedComponent from './components/AnimatedComponent';
+import {
+  mainAnimation,
+  descriptionAnimation,
+  logoAnimation,
+  buttonAnimation,
+} from '@/constants/animationSettings';
 
 function Landing() {
   return (
     <div className={styles['container']}>
-      <motion.div
-        className={styles['main']}
-        initial={{ opacity: 0, y: -50 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <div className={styles['description']}>
-          <motion.p
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 0.5 }}
-          >
-            내가 좋아하는 아이돌을 <br />
-            가장 <span>쉽게 덕질</span> 하는 방법
-          </motion.p>
-          <Link to='/list'>
-            <motion.img
-              src={logoImage}
-              alt='팬덤 케이'
-              width={509}
-              height={97}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1, delay: 1.5 }}
-            />
-          </Link>
-          <motion.div
-            className={styles['start-button']}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 1, delay: 1.5 }}
-          >
+      <AnimatedComponent animation={mainAnimation}>
+        <div className={styles['main']}>
+          <div className={styles['description']}>
+            <AnimatedComponent animation={descriptionAnimation}>
+              <p>
+                내가 좋아하는 아이돌을 <br />
+                가장 <span>쉽게 덕질</span> 하는 방법
+              </p>
+            </AnimatedComponent>
             <Link to='/list'>
-              <button>지금 시작하기</button>
+              <AnimatedComponent animation={logoAnimation}>
+                <img src={logoImage} alt='팬덤 케이' width={509} height={97} />
+              </AnimatedComponent>
             </Link>
-          </motion.div>
+            <AnimatedComponent animation={buttonAnimation}>
+              <div className={styles['start-button']}>
+                <Link to='/list'>
+                  <button>지금 시작하기</button>
+                </Link>
+              </div>
+            </AnimatedComponent>
+          </div>
         </div>
-      </motion.div>
+      </AnimatedComponent>
       <Description />
     </div>
   );

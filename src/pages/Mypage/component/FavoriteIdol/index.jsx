07af5377fba9ff64701b9e444b27b86eb0f56d07
KangHyoSung <godkingdawn@naver.com>
2024-06-20 23:10:20 +0900

    Feat: 추가하기 버튼 클릭시내가 관심있는 아이돌 실시간렌더링

--- src/pages/Mypage/component/FavoriteIdol/index.jsx
+++ src/pages/Mypage/component/FavoriteIdol/index.jsx
@@ -1,42 +1,4 @@
 
-
-// import styles from './styles.module.scss';
-// import logoImage from '@/assets/images/logo/logo.png';
-// import FavoriteCard from '../FavoriteCard';
-
-
-// function FavoriteIdol({ favoriteIdols, onSelect }) {
-
-//   return (
-//     <div className={styles['favorite-idol']}>
-//       <div className={styles['favorite-idol-title']}>
-//         <h3>내가 관심있는 아이돌</h3>
-//       </div>
-
-//       <div className={styles['favorite-card-container']}>
-//         {favoriteIdols.map((item) => (
-//           <FavoriteCard key={item.id} item={item} onSelect={onSelect} />
-//         ))}
-//       </div>
-//       <div className={styles['favorite-add-info']}>
-//         <img
-//           src={logoImage}
-//           className={styles['logo-image']}
-//           alt='로고 이미지'
-//           width='120px'
-//           height='23'
-//         />
-//         <h3>좋아하는 아이돌을 추가해주세요</h3>
-//       </div>
-
-//       <div className={styles['bottom-line']}></div>
-//     </div>
-//   );
-// }
-
-// export default FavoriteIdol;
-
-
 import styles from './styles.module.scss';
 import logoImage from '@/assets/images/logo/logo.png';
 import FavoriteCard from '../FavoriteCard';

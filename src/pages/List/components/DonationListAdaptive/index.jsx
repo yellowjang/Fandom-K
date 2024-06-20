c1a4ef2968421fd33433c5852eb24ab24a91e0d8
장아영 <jang-ayoung@jang-ayeong-ui-MacBookPro.local>
2024-06-20 15:05:44 +0900

    Fix: 후원하기 리스트 컴포넌트 반응형시 정렬 및 타이틀 이동 문제 해결

--- src/pages/List/components/DonationListAdaptive/index.jsx
+++ src/pages/List/components/DonationListAdaptive/index.jsx
@@ -8,7 +8,6 @@ import ModalPortal from '../Modal/components/ModalPortal';
 
 import useAsyncWithRetry from '@/hooks/useAsyncWithRetry';
 
-
 function DonationListAdaptive({
   isModalOpen,
   closeModal,
@@ -30,9 +29,9 @@ function DonationListAdaptive({
 
   return (
     <div className={styles['donation-list']}>
+      <p className={styles['list-title']}>후원을 기다리는 조공</p>
       <div className={styles['components-container']}>
         <div className={styles['donation-contents']}>
-          <p className={styles['list-title']}>후원을 기다리는 조공</p>
           <div className={styles['components-wrapper']}>
             {donations.map((donation) => (
               <DonationElement

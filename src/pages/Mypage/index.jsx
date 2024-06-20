5a99ede9fe63d9db111a856de350ec16173a63b3
kanghyosung1 <166676502+kanghyosung1@users.noreply.github.com>
2024-06-20 15:29:57 +0900

    Reafector:코드 오타수정 (#39)

--- src/pages/Mypage/index.jsx
+++ src/pages/Mypage/index.jsx
@@ -1,33 +1,3 @@
-// import { useState, useEffect } from 'react';
-// import styles from './styles.module.scss';
-// import FavoriteIdol from './component/FavoriteIdol';
-// import SelectIdolList from './component/SelectIdolList';
-// import { getIdols } from '@/services/api/idols';
-
-// function Mypage() {
-//   const [idols, setIdols] = useState([]);
-//   const [favoriteIdols, setFavoriteIols] = useState();
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const { list } = await getIdols({ pageSize: 100 });
-//       setIdols(list);
-//     };
-
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <div className={styles['mypage']}>
-//       <div className={styles['mypage-wrapper']}>
-//         <FavoriteIdol />
-//         <SelectIdolList key={idols.id} idols={idols} />
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default Mypage;
 
 
 import { useState, useEffect } from 'react';

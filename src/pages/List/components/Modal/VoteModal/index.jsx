d3b6dac861404fa97b72e9b940432a6f80fc898e
engdawn <155536270+engdawn@users.noreply.github.com>
2024-06-21 21:48:50 +0900

    Feat: 투표 모달 반응형 완성 (#50)

--- src/pages/List/components/Modal/VoteModal/index.jsx
+++ src/pages/List/components/Modal/VoteModal/index.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import styles from './styles.module.scss';
 import closeIcon from '@/assets/icons/ic_close.svg';
 import chartIcon from '@/assets/icons/ic_chart.svg';
+import checkIcon from '@/assets/icons/ic_check.svg';
+import backIcon from '@/assets/icons/icj_arrow_left.svg';
 import ModalChart from './ModalChart';
 import { disableScroll, activateScroll } from '../components/ModalScroll';
 import { postVotes } from '@/services/api/votes';
@@ -18,6 +20,7 @@ function VoteModal({ items, gender, setItems, setShouldRerender }) {
 
   const toggleModal = () => {
     setModal(!modal);
+    setCredit(localStorage.getItem('credits'));
   };
 
   const genderCheck = gender === 'female' ? '여자' : '남자';
@@ -30,6 +33,13 @@ function VoteModal({ items, gender, setItems, setShouldRerender }) {
       };
     }
   }, [modal]);
+
+  const [credit, setCredit] = useState('0');
+
+  useEffect(() => {
+    const currentCredits = localStorage.getItem('credits') || '0';
+    setCredit(currentCredits);
+  }, [credit]);
 
   const handleVote = async () => {
     if (selectedIdol) {
@@ -53,6 +63,18 @@ function VoteModal({ items, gender, setItems, setShouldRerender }) {
     setToastMessage('');
   };
 
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const widthMaxMobile = 767;
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <>
       <button onClick={toggleModal} className={styles['btn-modal']}>
@@ -71,7 +93,10 @@ function VoteModal({ items, gender, setItems, setShouldRerender }) {
             <header>
               <h2>이달의 {genderCheck} 아이돌</h2>
               <button onClick={toggleModal}>
-                <img src={closeIcon} alt='닫기 아이콘' />
+                <img
+                  src={windowWidth > widthMaxMobile ? closeIcon : backIcon}
+                  alt='닫기 아이콘'
+                />
               </button>
             </header>
             <main>
